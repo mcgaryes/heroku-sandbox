0a9149ddb4db2feaf92f6e39f057f88b38a1e0d1
Eric McGary <emcgary@resource.com>
2013-08-01 16:38:13 -0400

    Update to cloudinary functionality

--- server.js
+++ server.js
@@ -51,12 +51,12 @@ app.get('/upload', function(req, res) {
 });
 
 app.post('/upload', function(req, res, next) {
-	res.json(201, result);
-	res.send(format('\nuploaded %s (%d Kb) to %s as %s'
-    , req.files.file.name
-    , req.files.file.size / 1024 | 0 
-    , req.files.file.path
-    , req.body.title));
+	stream = cloudinary.uploader.upload_stream(function(result) {
+		res.json(201, result);
+	}, { public_id: req.body.title });
+	fs.createReadStream(req.files.image.path, {
+		encoding: 'binary'
+	}).on('data', stream.write).on('end', stream.end);
 	/*
 	if (req.files) {
 		var file = req.files.file;
